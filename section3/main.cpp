37b2ea298e1c9e417d0d867d423fae084afc6705
JM <j.maxwell.alan@gmail.com>
2021-09-03 17:28:09 -0400

    ramping up

--- section3/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#define log(x) std::clog << x << std::endl;
-
-int main()
-{
-  log("QUE BRUH");
-}
--- sorted.cpp
+++ sorted.cpp
@@ -1,30 +1,102 @@
-#include <stdlib.h>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
 
-using namespace std;
+#define log(x) std::clog << x << std::endl;
 
-vector<int> quickSort (vector<int> vect)
+void sort(std::vector<int> *ptrV)
 {
-  if (vect.size() < 2) return vect;
-  const int pivot = vect.back();
-  vect.pop_back();
-  const int size = vect.size();
-  vector<int> left, right, acc;
-  for (int i = 0; i < size; i++) vect[i] < pivot ? left.push_back(vect[i]) : right.push_back(vect[i]);
-  const vector<int> sortedLeft = quickSort(left), sortedRight = quickSort(right);
-  const int leftSize = sortedLeft.size(), rightSize = sortedRight.size();
-  for (int i = 0; i < leftSize; i++) acc.push_back(sortedLeft[i]);
-  acc.push_back(pivot);
-  for (int i = 0; i < rightSize; i++) acc.push_back(sortedRight[i]);
-  return acc;
+  std::sort(ptrV->begin(), ptrV->end());
 }
 
-int main ()
+void merge(std::vector<int> *ptrL, std::vector<int> *ptrR, std::vector<int> *ptrAcc)
 {
-  vector<int> unsorted;
-  for (int i = 0; i < 100; i++) unsorted.push_back(rand());
-  const vector<int> sorted = quickSort(unsorted);
-  for (int i = 0; i < 100; i++) cout << sorted[i] << "|";
-  cout << endl;
+  const size_t sizeL{(*ptrL).size()}, sizeR{(*ptrR).size()};
+  unsigned int posL{}, posR{};
+  while (posL < sizeL || posR < sizeR)
+  {
+    if (posL < sizeL && posR < sizeR)
+    {
+      if ((*ptrL)[posL] < (*ptrR)[posR])
+        ptrAcc->push_back((*ptrL)[posL++]);
+      else
+        ptrAcc->push_back((*ptrR)[posR++]);
+    }
+    else if (posL < sizeL)
+    {
+      std::vector<int>{}.swap((*ptrR));
+      for (; posL < sizeL; posL++)
+        ptrAcc->push_back((*ptrL)[posL]);
+      std::vector<int>{}.swap((*ptrL));
+    }
+    else
+    {
+      std::vector<int>{}.swap((*ptrL));
+      for (; posL < sizeL; posL++)
+        ptrAcc->push_back((*ptrL)[posL]);
+      std::vector<int>{}.swap((*ptrR));
+    }
+  }
+}
+
+void alloc(std::vector<int> *vect)
+{
+  for (int i = 0; i < 125000000; i++)
+    vect->push_back(rand());
+}
+
+int main()
+{
+  std::vector<int> a{}, b{}, c{}, d{}, e{}, f{}, g{}, h{}, sieve1{}, sieve2{}, sieve3{}, sieve4{}, sieve5{}, sieve6{}, acc{};
+  log("allocating");
+  const std::future<void>
+    allocA{std::async(std::launch::async, alloc, &a)},
+    allocB{std::async(std::launch::async, alloc, &b)},
+    allocC{std::async(std::launch::async, alloc, &c)},
+    allocD{std::async(std::launch::async, alloc, &d)},
+    allocE{std::async(std::launch::async, alloc, &e)},
+    allocF{std::async(std::launch::async, alloc, &f)},
+    allocG{std::async(std::launch::async, alloc, &g)};
+  alloc(&h);
+  allocG.wait();
+  allocF.wait();
+  allocE.wait();
+  allocD.wait();
+  allocC.wait();
+  allocB.wait();
+  allocA.wait();
+  log("allocating complete, sorting");
+  const std::future<void>
+    sortA{std::async(std::launch::async, sort, &a)},
+    sortB{std::async(std::launch::async, sort, &b)},
+    sortC{std::async(std::launch::async, sort, &c)},
+    sortD{std::async(std::launch::async, sort, &d)},
+    sortE{std::async(std::launch::async, sort, &e)},
+    sortF{std::async(std::launch::async, sort, &f)},
+    sortG{std::async(std::launch::async, sort, &g)};
+  sort(&h);
+  sortG.wait();
+  sortF.wait();
+  sortE.wait();
+  sortD.wait();
+  sortC.wait();
+  sortB.wait();
+  sortA.wait();
+  log("sort complete");
+  log("merging phase 1");
+  const std::future<void>
+    mergeAB{std::async(std::launch::async, merge, &a, &b, &sieve1)},
+    mergeCD{std::async(std::launch::async, merge, &c, &d, &sieve2)},
+    mergeEF{std::async(std::launch::async, merge, &e, &f, &sieve3)};
+  merge(&g, &h, &sieve4);
+  mergeEF.wait();
+  mergeCD.wait();
+  mergeAB.wait();
+  log("merging phase 2");
+  const std::future<void> merge12{std::async(std::launch::async, merge, &sieve1, &sieve2, &sieve5)};
+  merge(&sieve3, &sieve4, &sieve6);
+  merge12.wait();
+  log("merging phase 3");
+  merge(&sieve5, &sieve6, &acc);
 }
