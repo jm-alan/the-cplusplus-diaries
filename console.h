290ba48f96648eb3489bea8acc43591630c23111
jm <j.maxwell.alan@gmail.com>
2021-09-17 22:17:55 -0400

    adjust console::exec to braced initialization

--- console.h
+++ console.h
@@ -8,9 +8,9 @@ namespace console
 {
   std::string exec(const char *cmd)
   {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    std::array<char, 128> buffer{};
+    std::string result{};
+    std::unique_ptr<FILE, decltype(&pclose)> pipe{popen(cmd, "r"), pclose};
     if (!pipe)
       throw std::runtime_error("popen() failed!");
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
