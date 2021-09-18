6506000a2b5eb50ec291ddbf658ea7bec10dad43
jm <j.maxwell.alan@gmail.com>
2021-09-18 01:41:54 -0400

    refactor to destroy vectors as they are merged

--- sorted.cpp
+++ sorted.cpp
@@ -1,5 +1,3 @@
-// Need IO
-#include <iostream>
 // Vectors
 #include <vector>
 // Threading/async
@@ -12,6 +10,8 @@
 #include <random>
 // Timer
 #include <chrono>
+// Pause, print to screen, etc
+#include "console.h"
 
 // and a system time macro as well
 #define now() std::chrono::system_clock::now()
@@ -47,51 +47,49 @@ void merge(
     std::vector<unsigned long long> *ptrR,
     std::vector<unsigned long long> *ptrAcc)
 {
+  // Immediately resize the accumulator to fit the total number of elements that will be merged into it
+  ptrAcc->resize(ptrL->size() + ptrR->size());
 
-  // Storing the vector sizes in discrete variables is marginally faster than calling .size() constantly
-  const size_t sizeL{ptrL->size()}, sizeR{ptrR->size()};
-
-  // Independently track the iterator positions of the left and right vectors
-  int posL{}, posR{};
-
-  // If either one of the position trackers has not yet reached the size of its respective vectors, keep iterating
-  while (posL < sizeL || posR < sizeR)
+  size_t accPos{ptrAcc->size() - 1};
+  // If the size of both vectors is > 0
+  while (ptrL->size() || ptrR->size())
   {
-
-    // If BOTH haven't reached the end yet, then we know we can safetly key into both vectors to directly compare the values
-    if (posL < sizeL && posR < sizeR)
+    // If BOTH are still > 0 then we have values to compare
+    if (ptrL->size() && ptrR->size())
     {
-
       // Put the lesser of the two into the accumulator
-      if ((*ptrL)[posL] < (*ptrR)[posR])
-        ptrAcc->push_back((*ptrL)[posL++]);
+      if (ptrL->back() > ptrR->back())
+      {
+        (*ptrAcc)[accPos--] = ptrL->back();
+        ptrL->pop_back();
+      }
       else
-        ptrAcc->push_back((*ptrR)[posR++]);
+      {
+        (*ptrAcc)[accPos--] = ptrR->back();
+        ptrR->pop_back();
+      }
     }
 
     // If we've skipped the && above, then one of the two must have finished. If it's not this one, then we know the right must
     // have finished
-    else if (posL < sizeL)
+    else if (ptrL->size())
     {
-      // So we create an empty vector that will get destroyed when this frame comes off the stack
-      // and swap its contents with the contents of the vector we know we're finished with, effectively deallocating the memory
-      // when this stack frame is cleared
-      trash(ptrR);
-
       // We iterate through the remainder of the unfinished vector
-      for (; posL < sizeL; posL++)
-        ptrAcc->push_back((*ptrL)[posL]);
-      // and then dump its memory the same way
-      trash(ptrL);
+      while (ptrL->size())
+      {
+        (*ptrAcc)[accPos--] = ptrL->back();
+        ptrL->pop_back();
+      }
     }
 
-    // If we get here, we know that the opposite of the above is true and we can do the same steps in reverse
+    // If we get here, we know that the opposite of the above is true and we can do the same steps for the opposite vector
     else
     {
-      trash(ptrL);
-      for (; posR < sizeR; posR++)
-        ptrAcc->push_back((*ptrR)[posR]);
-      trash(ptrR);
+      while (ptrR->size())
+      {
+        (*ptrAcc)[accPos--] = ptrR->back();
+        ptrR->pop_back();
+      }
     }
   }
 }
@@ -101,10 +99,10 @@ void alloc(std::vector<unsigned long long> *vect, const int ceil)
 {
   std::random_device rd;
   std::default_random_engine generator(rd());
-  std::uniform_int_distribution<long long unsigned> distribution(0, 0xFFFFFFFFFFFFFFFF);
+  std::uniform_int_distribution<unsigned long long> distribution(0, 0xFFFFFFFFFFFFFFFF);
   for (int i = 0; i < ceil; i++)
-    // vect->push_back(rand());
     vect->push_back(distribution(generator));
+    // vect->push_back(rand());
 }
 
 int main()
@@ -112,62 +110,46 @@ int main()
   char again{};
   // Poll hardware for number of logical threads
   const unsigned int queriedCPUs{std::thread::hardware_concurrency()};
-  std::clog
-      << "Queried available logical processors: "
-      << queriedCPUs
-      << std::endl
-      << std::endl
-      << "Is this correct? [y/n] ";
+  console::inl("Queried available logical processors: ");
+  console::log(queriedCPUs);
+  console::log("Is this correct? [y/n]");
   char yesno;
-  int threads;
   std::cin >> yesno;
-  std::clog
-      << std::endl;
+  int threads;
   if (yesno != 'y' && yesno != 'Y')
   {
-    std::clog
-        << "Please input the correct number of threads available on your machine: ";
-    std::cin >> threads;
+    console::log("Please input the correct number of threads available on your machine: ");
+    std::cin >>
+        threads;
   }
   else
     threads = queriedCPUs;
+
   // Use some proprietary Windows C++ library garbage to query total system memory
   MEMORYSTATUSEX stax;
   stax.dwLength = sizeof(stax);
   GlobalMemoryStatusEx(&stax);
   const int physMem = stax.ullAvailPhys / (1024 * 1024);
   const int totMem = stax.ullTotalPhys / (1024 * 1024);
-  std::clog
-      << "Queried available memory in MB: "
-      << std::endl
-      << std::endl
-      << physMem
-      << " of "
-      << totMem
-      << std::endl
-      << std::endl
-      << "Is this roughly correct? [y/n] ";
+  console::inl("Queried available memory in MB: ");
+  console::inl(physMem);
+  console::inl(" of ");
+  console::log(totMem);
+  console::log("Is this roughly correct? [y/n]");
   std::cin >> yesno;
   int mem;
   if (yesno != 'y' && yesno != 'Y')
   {
-    std::clog
-        << std::endl
-        << std::endl
-        << "Please input the current amount of AVAILABLE (total - occupied) memory in your machine, in MB ";
+    console::log("Please input the current amount of AVAILABLE (total - occupied) memory in your machine, in MB:");
     std::cin >> mem;
   }
   else
     mem = physMem;
-  std::clog << std::endl;
   // Rough estimate that it takes about 19mb of RAM to allocate, sort, and merge 1,000,000 unsigned long longs
   int totalSort{(((mem / 19) * 1000000) / threads) * threads};
 
-  std::clog
-      << "Max sortable long ints: "
-      << totalSort
-      << std::endl
-      << std::endl;
+  console::inl("Max sortable long ints: ");
+  console::log(totalSort);
 
   do
   {
@@ -189,10 +171,7 @@ int main()
     for (int i = 0; i < threads; i++)
       vects.push_back(new std::vector<unsigned long long>{});
 
-    std::clog
-        << "Allocating vector space"
-        << std::endl
-        << std::endl;
+    console::log("Allocating vector space");
     // Start an absolute and per-area timer
     const std::chrono::system_clock::time_point globalTimer{now()};
     std::chrono::system_clock::time_point runningTimer{globalTimer};
@@ -207,22 +186,17 @@ int main()
     // Wait for all of the allocation futures to resolve before continuing
     for (int i = 0; i < allocators.size(); i++)
       allocators[i].wait();
-    std::clog
-        << "Allocation completed in "
-        << timerInMS(runningTimer)
-        << " milliseconds"
-        << std::endl
-        << std::endl;
+
+    console::inl("Allocation completed in ");
+    console::inl(timerInMS(runningTimer));
+    console::log(" milliseconds");
 
     // Dump the vector of resolved allocation futures
     trash(&allocators);
 
     // Bring the timer up to date
     syncWatch(runningTimer);
-    std::clog
-        << "Let the sorting begin..."
-        << std::endl
-        << std::endl;
+    console::log("Let the sorting begin...");
 
     // Identical process for creating the allocation threads, only for creating sorts now
     for (int i = 0; i < (threads - 1); i++)
@@ -235,19 +209,11 @@ int main()
     for (int i = 0; i < sorters.size(); i++)
       sorters[i].wait();
 
-    std::clog
-        << "Sort completed in "
-        << timerInMS(runningTimer)
-        << " milliseconds"
-        << std::endl
-        << std::endl;
+    console::inl("Sort completed in ");
+    console::inl(timerInMS(runningTimer));
+    console::log(" milliseconds");
 
-    std::clog
-        << "Beginning merge."
-        << std::endl
-        << "(memory usage will spike sporadically)"
-        << std::endl
-        << std::endl;
+    console::log("Beginning merge. Memory usage will spike sporadically.");
 
     syncWatch(runningTimer);
 
@@ -278,7 +244,6 @@ int main()
     // then we know that we must continue merging
     while (sieves[sieves.size() - 1]->size() < totalSort)
     {
-
       // We store the size of the sieves vector prior to pushing anything into it so that we don't loop infinitely
       const size_t sieveSizeNow = sieves.size();
 
@@ -304,38 +269,29 @@ int main()
     // Theoretically, when this while loop merge has finished, we've successfully merged all of our sorts together into one massive
     // vector, and we're finished.
 
-    std::clog
-        << "Merge completed in "
-        << timerInMS(runningTimer)
-        << " milliseconds"
-        << std::endl
-        << std::endl;
+    console::log("Merge completed in");
+    console::log(timerInMS(runningTimer));
+    console::log("milliseconds");
 
     const auto finalTimer = timerInMS(globalTimer);
 
-    std::clog
-        << "Total operation for allocating, sorting, and merging "
-        << totalSort
-        << " unsigned long long integers completed in "
-        << finalTimer
-        << " milliseconds"
-        << std::endl
-        << "for a total average rate of "
-        << totalSort / finalTimer
-        << " ints/ms"
-        << std::endl
-        << std::endl;
+    console::log("Total operation for allocating, sorting, and merging completed in");
+    console::log(finalTimer);
+    console::log("milliseconds");
+    console::log("for a total average rate of");
+    console::log(totalSort / finalTimer);
+    console::log("ints/ms");
 
     // Uncomment the three lines below if you want to print the sorted vector when it's finished
     acc.swap(*(sieves[sieves.size() - 1]));
-    // const size_t accSize = acc.size();
-    // for (int i = 0; i < accSize; i++)
-    //   std::clog << acc[i]);
+    const size_t accSize = acc.size();
+    for (int i = 0; i < accSize; i++)
+      console::log(acc[i]);
     trash(&acc);
-    std::cout << "Run again? [y/n] ";
+    console::log("Run again? [y/n] ");
     std::cin >> again;
     std::cout << std::endl;
   } while (again == 'y' || again == 'Y');
 
-  system("pause");
+  console::pause();
 }
