c7deeb6c8f95404e31667c7108fa1abb09f14c5e
jm <j.maxwell.alan@gmail.com>
2021-10-10 19:09:42 -0400

    remove console::exec

--- console.h
+++ console.h
@@ -1,25 +1,8 @@
 #include <iostream>
 #include <iomanip>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
 
 namespace console
 {
-  std::string exec(const char *cmd)
-  {
-    // Taken from this absolutley wonderful SO post https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
-    std::array<char, 128> buffer{};
-    std::string result{};
-    std::unique_ptr<FILE, decltype(&pclose)> pipe{popen(cmd, "r"), pclose};
-    if (!pipe)
-      throw std::runtime_error("popen() failed!");
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-      result += buffer.data();
-    return result;
-  }
-
   void inl(const auto msg)
   {
     std::clog << msg;
