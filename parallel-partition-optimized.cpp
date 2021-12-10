c61cfdf7994508f8ff3342746aabc20809ac12a1
jm <j.maxwell.alan@gmail.com>
2021-12-10 16:47:25 -0500

    a partitioner that works maybe

--- parallel-partition-optimized.cpp
+++ parallel-partition-optimized.cpp
@@ -1,5 +1,6 @@
 #include <future>
 #include <vector>
+#include <algorithm>
 
 const std::vector<unsigned long long> partition(
     unsigned long long *ptrArr,
@@ -8,16 +9,38 @@ const std::vector<unsigned long long> partition(
     const unsigned int numPivots,
     std::vector<std::mutex *> sectionLocks)
 {
+  std::vector<unsigned long long> partIdxs{};
   // cannot std::lock_guard bc lock_guard cannot be itself put into a vector
   // due to copy/move constraints
   for (size_t i{}; i < sectionLocks.size(); i++)
     sectionLocks[i]->lock();
-  std::vector<unsigned long long> pivots{};
+  std::sort(ptrArr + (end - numPivots), ptrArr + end);
+  size_t breakPoint{start}, validPoint{};
+  unsigned long long staging{};
   for (unsigned int i{}; i < numPivots; i++)
-    pivots.push_back(ptrArr[end - i]);
-
+  {
+    unsigned long long pivotIdx{end - (numPivots - i)};
+    const unsigned long long pivot{ptrArr[pivotIdx]};
+    validPoint = breakPoint;
+    while (validPoint < (end - (numPivots - i)))
+    {
+      if (ptrArr[validPoint] < pivot)
+      {
+        staging = ptrArr[breakPoint];
+        ptrArr[breakPoint] = ptrArr[validPoint];
+        ptrArr[validPoint] = staging;
+        breakPoint++;
+      }
+      validPoint++;
+    }
+    partIdxs.push_back(breakPoint);
+    staging = ptrArr[breakPoint];
+    ptrArr[breakPoint] = ptrArr[pivotIdx];
+    ptrArr[pivotIdx] = staging;
+  }
   for (size_t i{}; i < sectionLocks.size(); i++)
     sectionLocks[i]->unlock();
+  return partIdxs;
 };
 
 int main()
