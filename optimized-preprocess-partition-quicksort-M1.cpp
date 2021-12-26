4a4945077fd8b04e79149dfbf9a5177d4163b585
jm <j.maxwell.alan@gmail.com>
2021-12-25 21:27:29 -0500

    comments

--- optimized-preprocess-partition-quicksort-M1.cpp
+++ optimized-preprocess-partition-quicksort-M1.cpp
@@ -1,10 +1,11 @@
-#include <future>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <iostream>
-#include <chrono>
+#include <future>    // threads
+#include <vector>    // dynamic arrays
+#include <algorithm> // std::sort
+#include <random>    // for creating random number generators
+#include <iostream>  // terminal input / output
+#include <chrono>    // timer
 
+// macros (illegal, I know) to manage the timer
 #define sync(timer) timer = std::chrono::system_clock::now()
 #define timer_cast_ms(timer)                                 \
   (                                                          \
@@ -12,10 +13,23 @@
           std::chrono::system_clock::now() - timer))         \
       .count()
 
+// Instantiate random device & range of numbers to be generated
 std::random_device rd{};
-std::default_random_engine generator{rd()};
+std::default_random_engine generator{rd()}; //
 std::uniform_int_distribution<unsigned int> distribution{0, 0xFFFFFFFF};
 
+// The partition function, generally speaking, does the pivot step
+// of quickSort, without recursing
+// There are 3 overloads; one where the start & end are concrete,
+// one each where either the start or end may be std::shared_future
+// It's been genericized to take in an arbitrary number of desired
+// pivot points, all of which are selected from the end of the array
+// It will use these pivots "blindly" i.e. no consideration is given
+// to how well those pivots represent the distribution of numbers
+// within the selection of the array being partitioned
+// It returns a vector of the indicies where the partitions land
+
+// Known start + end
 std::vector<unsigned long long> partition(
     unsigned int *ptrArr,
     const unsigned long long start,
@@ -50,6 +64,7 @@ std::vector<unsigned long long> partition(
   return partIdxs;
 };
 
+// Known start, possibly unknown end
 std::vector<unsigned long long> partition(
     unsigned int *ptrArr,
     const unsigned long long start,
@@ -85,6 +100,7 @@ std::vector<unsigned long long> partition(
   return partIdxs;
 };
 
+// Possibly unknown start, known end
 std::vector<unsigned long long> partition(
     unsigned int *ptrArr,
     std::shared_future<std::vector<unsigned long long>> futStart,
@@ -120,6 +136,7 @@ std::vector<unsigned long long> partition(
   return partIdxs;
 };
 
+// This is just fully in-place quicksort
 void sort_serial(
     unsigned int *ptrArr,
     const unsigned long long start,
@@ -148,6 +165,18 @@ void sort_serial(
   sort_serial(ptrArr, breakPoint + 1, end);
 };
 
+// The sort_concurrent is identical to sort_serial except that
+// it exists in an overload set spanning the different permutations
+// of known & unknown start & end indicies
+// The unknown indicies are passed in from a vector, since that
+// is the return value of the partition function
+// They are wrapped in a lambda to pass overloads to std::async,
+// so all overloads must take in 5 arguments even though in
+// the case of only having 1 future passed in, only one of the
+// index arguments will be used. In those cases, the 4th argument
+// is taken as the selecting index, and the 5th argument is discarded.
+
+// Known start, possibly unknown end, 1 index, 1 waste
 void sort_concurrent(
     unsigned int *ptrArr,
     const unsigned long long start,
@@ -179,6 +208,7 @@ void sort_concurrent(
   sort_serial(ptrArr, breakPoint + 1, end);
 };
 
+// Possibly unknown start, known end, 1 index, 1 waste
 void sort_concurrent(
     unsigned int *ptrArr,
     std::shared_future<std::vector<unsigned long long>> futStart,
@@ -210,6 +240,7 @@ void sort_concurrent(
   sort_serial(ptrArr, breakPoint + 1, end);
 };
 
+// Possibly unknown start & end, both indicies used
 void sort_concurrent(
     unsigned int *ptrArr,
     std::shared_future<std::vector<unsigned long long>> futStart,
@@ -243,7 +274,8 @@ void sort_concurrent(
   sort_serial(ptrArr, breakPoint + 1, end);
 };
 
-void alloc(
+// Inserts random numbers ptrArr:[start, end)
+void randomize(
     unsigned int *ptrArr,
     const unsigned long long start,
     const unsigned long long end)
@@ -263,85 +295,157 @@ bool is_sorted(
   return true;
 }
 
+// Lambda wrapper for partition function to clean up repeatedly
+// writing futures.push_back(std::async etc etc etc)
+const auto async_partition =
+    [](auto to_part, auto start, auto end, auto num_parts)
+{
+  return std::async(
+      std::launch::async,
+      [](auto one, auto two, auto three, auto four)
+      { return partition(one, two, three, four); },
+      to_part,
+      start,
+      end,
+      num_parts);
+};
+
+// Lambda wraper for sort function for the same reason as the partition
+const auto async_sort =
+    [](auto to_sort, auto start, auto end, auto posStartIdx, auto posEndIdx)
+{
+  return std::async(
+      std::launch::async,
+      [](auto one, auto two, auto three, auto four, auto five)
+      { sort_concurrent(one, two, three, four, five); },
+      to_sort,
+      start,
+      end,
+      posStartIdx,
+      posEndIdx);
+};
+
 int main()
 {
+  // Hopefully returns the number of threads on the system
   const unsigned int threads{std::thread::hardware_concurrency()};
-  unsigned int parts{threads};
+
+  // To hold the sort futures so they don't auto-destroy
   std::vector<std::shared_future<void>> futures{};
+
+  // How big is this array going to be?
   unsigned long long ceil{};
   std::cin >> ceil;
   std::clog << std::endl;
-  const unsigned long long allocSectionSize{ceil / threads};
-  const unsigned long long cap{allocSectionSize * threads};
+
+  // Divide the array into equally sized sections
+  const unsigned long long randomizeSectionSize{ceil / threads};
+
+  // Set the array length to section size * original divider,
+  // intentionally possibly rounding down to avoid the sections
+  // postentially not covering the entire array
+  const unsigned long long cap{randomizeSectionSize * threads};
+
+  // Start the timer
   auto timer{std::chrono::system_clock::now()};
+
+  // Copy it to track the entire runtime
   const auto globalTimer{timer};
+
+  // Allocate the array
   unsigned int *ints{new unsigned int[cap]{}};
-  const auto l_part =
-      [](auto one, auto two, auto three, auto four)
-  {
-    return partition(one, two, three, four);
-  };
-  const auto l_sort =
-      [](auto one, auto two, auto three, auto four, auto five)
-  {
-    sort_concurrent(one, two, three, four, five);
-  };
-  const auto async_l_sort =
-      [l_sort](auto to_sort, auto start, auto end, auto posStartIdx, auto posEndIdx)
-  {
-    return std::async(
-        std::launch::async,
-        l_sort,
-        to_sort,
-        start,
-        end,
-        posStartIdx,
-        posEndIdx);
-  };
   std::clog << "Allocated array in "
             << timer_cast_ms(timer) << "ms" << std::endl;
+
+  // Sync only the non-global timer
   sync(timer);
+
+  // Spawn threads to randomize individual sections of the array
   for (unsigned int i{}; i < threads; i++)
   {
     futures.push_back(
         std::async(
             std::launch::async,
-            alloc,
+            randomize,
             ints,
-            allocSectionSize * i,
-            allocSectionSize * (i + 1)));
+            randomizeSectionSize * i,
+            randomizeSectionSize * (i + 1)));
   }
+
+  // Wait for 100% of the randomizers to complete before beginning
+  // partitioning, since the first partition operates on the entire
+  // array
   for (size_t i{}; i < futures.size(); i++)
     futures[i].wait();
   std::clog << "Randomized array in " << timer_cast_ms(timer) << "ms" << std::endl;
   sync(timer);
-  const unsigned long long partOne{l_part(ints, 0, cap, 1)[0]};
+
+  // Split the array into two parts, producing a vector of length 1,
+  // whose sole element we grab immediately
+  const unsigned long long partition_one{partition(ints, 0, cap, 1)[0]};
+
+  // Create two threads, each partitioning one of the two regions
+  // created by the first partition
+  // The M1 Max has 10 cores (without regard for performance vs efficiency),
+  // so we seek in total to produce 10 partitions, or 9 dividers.
+  // Each one of these will produce 5 partitions split by 4 dividers, on
+  // either side of the original divider, for a total of 10 partitions
   std::shared_future<std::vector<unsigned long long>>
-      futPartTwo{std::async(std::launch::async, l_part, ints, 0, partOne, 4)},
-      futPartThree{std::async(std::launch::async, l_part, ints, partOne, cap, 4)};
-  futures.push_back(async_l_sort(ints, 0, futPartTwo, 0, 0));
-  futures.push_back(async_l_sort(ints, futPartTwo, futPartTwo, 0, 1));
-  futures.push_back(async_l_sort(ints, futPartTwo, futPartTwo, 1, 2));
-  futures.push_back(async_l_sort(ints, futPartTwo, futPartTwo, 2, 3));
-  futures.push_back(async_l_sort(ints, futPartTwo, partOne, 3, 0));
-  futures.push_back(async_l_sort(ints, partOne, futPartThree, 0, 0));
-  futures.push_back(async_l_sort(ints, futPartThree, futPartThree, 0, 1));
-  futures.push_back(async_l_sort(ints, futPartThree, futPartThree, 1, 2));
-  futures.push_back(async_l_sort(ints, futPartThree, futPartThree, 2, 3));
-  futures.push_back(async_l_sort(ints, futPartThree, cap, 3, 0));
+      f_partition_two{async_partition(ints, 0, partition_one, 4)},
+      f_partition_three{async_partition(ints, partition_one, cap, 4)};
+
+  // Spawn a thread, individually sorting each of the partitions
+  // The partitioning has effectively macro-sorted the array into 10
+  // sections which are in order in terms of their rangest; the largest
+  // value of partition 3 is guaranteed to be less than or equal to the
+  // smallest value of partition 4, etc. So, if we now sort every individual
+  // slice of the array, when the dust settles, the entire thing will be
+  // sorted.
+  // We don't have to worry about waiting for the previous two partition
+  // futures to resolve, since we call .get on them within the sort function.
+  // This works something like a mutex, but without me having to come up with
+  // some sort of wild system for managing all of the mutexes and dynamically
+  // assigning what section of the array they monitor.
+  // Plus, in this particular case, literally all of the threads sorting the
+  // "left" side of the array have to wait for f_partition_two, and all of
+  // the threads on the right have to wait for f_partition_three, since it's
+  // otherwise impossible to guarantee the individual section that thread will
+  // be sorting will have had its range appropriately constrained beforehand
+  futures.push_back(async_sort(ints, 0, f_partition_two, 0, 0));
+  futures.push_back(async_sort(ints, f_partition_two, f_partition_two, 0, 1));
+  futures.push_back(async_sort(ints, f_partition_two, f_partition_two, 1, 2));
+  futures.push_back(async_sort(ints, f_partition_two, f_partition_two, 2, 3));
+  futures.push_back(async_sort(ints, f_partition_two, partition_one, 3, 0));
+  futures.push_back(async_sort(ints, partition_one, f_partition_three, 0, 0));
+  futures.push_back(async_sort(ints, f_partition_three, f_partition_three, 0, 1));
+  futures.push_back(async_sort(ints, f_partition_three, f_partition_three, 1, 2));
+  futures.push_back(async_sort(ints, f_partition_three, f_partition_three, 2, 3));
+  futures.push_back(async_sort(ints, f_partition_three, cap, 3, 0));
+
+  // Then we wait for all of the sorting to finish
   for (size_t i{}; i < futures.size(); i++)
-    futures[i]
-        .wait();
+    futures[i].wait();
+
+  // We clock the total sort time and store it separately so that we can use it
+  // for calculations
   const auto sortTime{timer_cast_ms(timer)};
   std::clog
       << "Partitioned and sorted array in " << sortTime << "ms" << std::endl
       << "for a total processing speed of " << cap / sortTime << " ints/ms" << std::endl;
+
+  // Sync the timer AFTER printing to the console so that the is_sorted function is
+  // not unduly punished by the amount of time it takes to do IOps
   sync(timer);
+
+  // Validate the sort and clock the validation time     vvvvvvvvvvvvvvvvvv
   std::clog << "Array is sorted: " << std::boolalpha << is_sorted(ints, cap) << std::endl;
+  //                                    vvvvvvvvvvvvvvvvvvv
   std::clog << "Validated array in " << timer_cast_ms(timer) << "ms" << std::endl;
+
+  // Clock the global timer after everything is finished
   const auto finalTime{timer_cast_ms(globalTimer)};
   std::clog
-      << "Total process for allocating, partitioning, sorting, and verifying " << std::endl
+      << "Total process for randomizeating, partitioning, sorting, and verifying " << std::endl
       << cap << " 32-bit unsigned ints completed in " << finalTime << "ms" << std::endl
       << "for a total speed of " << (cap / finalTime) << " ints/ms" << std::endl;
 }
